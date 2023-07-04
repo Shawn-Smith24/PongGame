288250c8abe2f8730f93dc9c9bab856f9a9b1519
shawn <shawnsmith0024@gmail.com>
2023-07-03 20:31:32 -0400

    ball methods setup

--- src/Ball.java
+++ src/Ball.java
@@ -6,4 +6,16 @@ public class Ball {
     Ball(){
 
     }
+    public void setXDirection(int randomXDirection){
+
+    }
+    public void setYDirection(int randomYDirection){
+
+    }
+    public void move(){
+
+    }
+    public void draw(Graphics g){
+
+    }
 }
