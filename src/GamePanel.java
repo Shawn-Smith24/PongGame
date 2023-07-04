07b8522e424a89b85e7069bf1a0563a6f1dacb4d
shawn <shawnsmith0024@gmail.com>
2023-07-03 21:00:12 -0400

    panel methods updated

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 
 public class GamePanel extends JPanel implements Runnable{
     static final int GAME_WIDTH = 1000;
-    static final int GAME_HEIGHT = (int)(GAME_WIDTH * (5/9));
+    static final int GAME_HEIGHT = (int)(GAME_WIDTH * (0.5555));
     static final Dimension SCREEN_SIZE = new Dimension(GAME_WIDTH, GAME_HEIGHT);
     static final int BALL_DIAMTER = 20;
     static final int PADDLE_WIDTH = 25;
@@ -19,6 +19,15 @@ public class GamePanel extends JPanel implements Runnable{
     Score score;
 
     GamePanel(){
+        newPaddles();
+        newBall();
+        score = new Score(GAME_WIDTH, GAME_HEIGHT);
+        this.setFocusable(true);
+        this.addKeyListener(new AL());
+        this.setPreferredSize(SCREEN_SIZE);
+
+        gameThread = new Thread(this);
+        gameThread.start();
     }
     public void newBall(){
 
@@ -28,6 +37,10 @@ public class GamePanel extends JPanel implements Runnable{
     }
 
     public void paint(Graphics g){
+        image = createImage(getWidth(), getHeight());
+        graphics = image.getGraphics();
+        draw(graphics);
+        g.drawImage(image,0,0,this);
 
     }
     public void draw(Graphics g){
@@ -42,6 +55,7 @@ public class GamePanel extends JPanel implements Runnable{
 
     }
     public void run(){
+        //game loop
 
     }
     public class AL extends KeyAdapter{
